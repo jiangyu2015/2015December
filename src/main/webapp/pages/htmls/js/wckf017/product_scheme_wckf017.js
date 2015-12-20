880375b1f6f8e25d4e6d81027a4300f58d7fda1d
jiangyukun <187_6810_5877@sina.com>
2015-12-20 16:45:34 +0800

    微车 活动 名称演示update

--- src/main/webapp/pages/htmls/js/wckf017/product_scheme_wckf017.js
+++ src/main/webapp/pages/htmls/js/wckf017/product_scheme_wckf017.js
@@ -4,5 +4,39 @@
 
 +function () {
     webScroll.init({control: false});
-    //rtH5.init();
+
+    var $page1Whereby1 = $('.page1-whereby-1');
+    var $page1Whereby2 = $('.page1-whereby-2');
+    var $page1Whereby3 = $('.page1-whereby-3');
+    var $page1ListenToYou1 = $('.page1-listen-to-you-1');
+    var $page1ListenToYou2 = $('.page1-listen-to-you-2');
+
+    var animationCss = {
+        animation: ':fadeIn .5s'
+    };
+    var showCss = {
+        opacity: '1'
+    };
+
+    var start = 0, delta = 500;
+
+    animation($page1Whereby1, start, start + delta);
+    start += delta;
+    animation($page1Whereby2, start, start + delta);
+    start += delta;
+    animation($page1Whereby3, start, start + delta);
+    start += delta;
+    animation($page1ListenToYou2, start, start + delta);
+    start += delta;
+    animation($page1ListenToYou1, start, start + delta);
+
+    function animation($ele, start, end) {
+        setTimeout(function () {
+            $ele.css(animationCss);
+        }, start);
+        setTimeout(function () {
+            $ele.css(showCss);
+        }, end);
+    }
+
 }();
